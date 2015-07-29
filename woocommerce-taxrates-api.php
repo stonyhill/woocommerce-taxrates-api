e09a4770eee242bcca578fc643f2ce9cecaaad95
Steven Dunston <stevendunston@gmail.com>
2015-07-29 14:42:24 -0700

    Create woocommerce-taxrates-api.php

---
+++ woocommerce-taxrates-api.php
@@ -0,0 +1,8 @@
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+/**
+ * Check if WooCommerce is active
+ **/
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    // Put your plugin code here
+}
